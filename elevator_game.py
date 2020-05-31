0dea143b6cb293507efef5c16c6b01c79e624bff
grabania <grabania.work@gmail.com>
2020-05-31 23:28:27 +0100

    Update the third floor

--- elevator_game.py
+++ elevator_game.py
@@ -4,7 +4,7 @@ items = []
 
 def print_pause(message_to_print):
     print(message_to_print)
-    time.sleep(2)
+    time.sleep(1)
 
 
 print_pause("You have just arrived at your new job!")
@@ -49,5 +49,25 @@ while True:
         print_pause("You push the button for the third floor.")
         print_pause("After a few moments, you find yourself "
                     "in the engineering department.")
+        print_pause("This is where you work!")
+        if "ID card" in items:
+            print_pause("You use your ID card to open the door.")
+            print_pause("Your program manager greets you and tells "
+                        "you that you need to have a copy of the "
+                        "employee handbook in order to start work.")
+            if "handbook" in items:
+                print_pause("Fortunately, you got that from HR!")
+                print_pause("Congratulatons! You are ready to start your new job "
+                            "as vice president of engineering!")
+                break
+            else:
+                print_pause("They scowl when they see that you don't have it, "
+                            "and send you back to the elevator.")
+        else:
+            print_pause("Unfortunately, the door is locked "
+                        "and you can't get in.")
+            print_pause("It looks like you need some kind of "
+                        "key card to open the door.")
+            print_pause("You head back to the elevator.")
 
     print_pause("Where would you like to go next?")
